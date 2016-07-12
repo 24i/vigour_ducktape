742dc182b394568acfd9f58c52f832ecf677d5d9
Youri Daamen <me@youz.io>
2016-07-12 21:56:57 +0200

    initial version

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict'
+const fs = require('fs')
+const semver = require('semver')
+const test = require('tape')
+
+fs.readFile('package.json', (err, data) => {
+  if (err) { throw err }
+  const pkg = JSON.parse(data)
+  const deps = pkg.dependencies
+  var cnt = 0
+
+  test('npm fields', (t) => {
+    t.ok(pkg.name, `name: ${pkg.name}`)
+    t.ok(pkg.description, `description: ${pkg.description}`)
+    t.ok(semver.valid(pkg.version), `semver version: ${pkg.version}`)
+    t.end()
+  })
+
+  test('npm scripts', (t) => {
+    t.ok(pkg.scripts.start, `start: ${pkg.scripts.start}`)
+    t.ok(pkg.scripts.test, `description: ${pkg.scripts.test}`)
+    t.ok(pkg.scripts.travis, `travis: ${pkg.scripts.travis}`)
+    t.ok(pkg.scripts.cover, `cover: ${pkg.scripts.cover}`)
+    t.end()
+  })
+
+  test('npm dependencies', (t) => {
+    exec('readdir', 'node_modules', (files) => {
+      for (let i = files.length - 1; i >= 0; i--) {
+        const module = files[i]
+        if (module[0] === '@') {
+          exec('readdir', `node_modules/${module}`, (files) => {
+            for (let i = files.length - 1; i >= 0; i--) {
+              checkpackage(`node_modules/${module}/${files[i]}/package.json`, deps)
+            }
+          })
+        } else {
+          checkpackage(`node_modules/${module}/package.json`, deps)
+        }
+      }
+    })
+    function checkpackage (deppackage, deps) {
+      exec('readFile', deppackage, (data) => {
+        const pkg = JSON.parse(data)
+        const requiredVersion = deps[pkg.name]
+        if (requiredVersion) {
+          exec('readFile', deppackage, (data) => {
+            const version = JSON.parse(data).version
+            t.ok(semver.satisfies(version, requiredVersion), `${pkg.name}@${requiredVersion} (${version})`)
+          })
+        }
+      })
+    }
+    function exec (fn, file, cb) {
+      cnt++
+      fs[fn](file, (err, data) => {
+        if (!err) { cb(data) }
+        if (!--cnt) { t.end() }
+      })
+    }
+  })
+})
+ // has description
+ // blacklisted modules (hyperquest)
+ // folder structurte
+ // does it have coverage
+ // does it have travis
